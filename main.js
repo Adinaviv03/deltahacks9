e0ca79c3b94fd0bbdfbb9a2672a242122adc6175
mansha71 <mansharm71@gmail.com>
2023-01-14 15:51:13 -0500

    message

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
+task = []
+time = []
 
-function myFunction(){
-    let task = document.querySelector("#task");
-     
-}
+const mainDiv = document.querySelector('#mainDiv')
+
+
+button.onclick = function() {
+  var output = document.createElement('li');
+  output.innerHTML = document.getElementById('task').value + '  --  ' + document.getElementById('time').value;
+  task.push(document.getElementById('task').value)
+  time.push(document.getElementById('time').value)
+  document.body.appendChild(output);
+};
+document.body.appendChild(button);
