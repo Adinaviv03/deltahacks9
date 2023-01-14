5a455dfde403334377fcef5e76ff39f53fa79e57
ritual-17 <pattee14@mcmaster.ca>
2023-01-14 14:26:40 -0500

    Initial commit

---
+++ pk/day.py
@@ -0,0 +1,10 @@
+class day:
+    
+    start = 0
+    end = 24
+    numBlocks = 24
+
+    def __init__(self, startTime, endTime):
+        self.start = startTime
+        self.end = endTime
+        self.numBlocks = self.end - self.start
