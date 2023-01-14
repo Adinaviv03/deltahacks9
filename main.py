58e6336813b227af1b02ee1f39d7dcf0ab023b1c
ritual-17 <pattee14@mcmaster.ca>
2023-01-14 14:58:16 -0500

    Initial commit

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from pk.day import day
 class main:
     print("hello world")
     day1 = day(8, 17)
-    print("My day starts at", day1.start , "and ends at", day1.end)
+    print("My day starts at", day1.displayTime(day1.start) , "and ends at", day1.displayTime(day1.end))
+    print("I have", day1.numBlocks, "blocks available today!")
