474f2ac1c200599edc7add4bc3e04d2d894ea768
HussainM2003 <92009085+HussainM2003@users.noreply.github.com>
2023-01-14 18:41:28 -0500

    - updated mandatory variable in task class

--- pk/task.py
+++ pk/task.py
@@ -2,7 +2,9 @@ class task:
 
     name = ""
     length = 1 # 30 mins
+    time = 0
     isBreak = True
+    isMandatory = False
 
     def __init__(self) -> None:
         pass
@@ -11,4 +13,11 @@ class task:
         self.name = name
         self.length = length*2 ##converts to half hour blocks
         self.isBreak = False
+
+    def __init__(self, name, length, time):
+        self.name = name
+        self.length = length*2 ##converts to half hour blocks
+        self.time = time
+        self.isBreak = False
+        self.isMandatory = True
 
