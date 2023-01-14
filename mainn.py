c8abd86a60935fb36e5fd783c019f983092c72ea
ritual-17 <pattee14@mcmaster.ca>
2023-01-14 18:58:58 -0500

    updated main

--- mainn.py
+++ mainn.py
@@ -1,7 +1,17 @@
-from pk.day import day
+from pk import day, task
 
-class main:
+
+
+class mainn:
     print("hello world")
-    day1 = day(8, 17)
+    day1 = day(12, 19)
     print("My day starts at", day1.displayTime(day1.start) , "and ends at", day1.displayTime(day1.end))
     print("I have", day1.numBlocks, "blocks available today!")
+    task1 = task("walk dogs", 1)
+    task2 = task("clean house", 3)
+    task3 = task("get groceries", 2)
+    day1.addTask(task1)
+    day1.addTask(task2)
+    day1.addTask(task3)
+    day1.createSchedule
+    day1.displaySchedule
