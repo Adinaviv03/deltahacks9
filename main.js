4ce29f26d2c9afa1a6490ccf2def98f2940c02d0
mansha71 <mansharm71@gmail.com>
2023-01-15 10:35:19 -0500

    m

--- main.js
+++ main.js
@@ -140,6 +140,10 @@ class Day {
 let finalDay
 
 function makeDay(){
+    if((timeSet()[0][3] != '0' || timeSet()[0][3] != '3') && (timeSet()[0][4] != '0')){
+        alert("Please enter a time ending in 00 or 30");
+    }
+    else if((timeSet()[1][3] != '0' || timeSet()[1][3] != '3') && (timeSet()[1][4] != '0'))
     alert("Time Set");
     finalDay = new Day(timeSet()[0], timeSet()[1]);
 }
